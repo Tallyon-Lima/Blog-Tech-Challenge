8178a8eef84b8732d63b3c0f68ce324f0e7260db
Tallyon <tallyon.lima@sptech.school>
2026-06-25 22:09:15 -0300

    Correção do arquivo .sql

--- schema.sql
+++ schema.sql
@@ -7,28 +7,25 @@ CREATE TABLE IF NOT EXISTS post (
     disciplina VARCHAR(255) NOT NULL,
     data_criacao TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     data_atualizacao TIMESTAMP WITHOUT TIME ZONE NOT NULL
-)
+);
 
 CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NUll,
     senha VARCHAR(255) NOT NULL
-)
-
+);
 
 CREATE TABLE IF NOT EXISTS perfil_acesso (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR(255) NOT NULL
-)
+);
 
 alter table post 
 ADD COLUMN IF NOT EXISTS autor bigint not null;
 
-
 alter table usuarios
 ADD COLUMN IF NOT EXISTS perfil_id bigint not NULL;
-
 
 INSERT INTO public.perfil_acesso (nome)
 VALUES
