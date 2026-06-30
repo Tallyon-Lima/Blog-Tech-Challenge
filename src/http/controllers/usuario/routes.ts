19ca0c811f7c97cfd4b67c903ddffa083308f5ca
Danilo <daniloluglisantos@gmail.com>
2026-06-30 20:07:17 -0300

    route signin

--- src/http/controllers/usuario/routes.ts
+++ src/http/controllers/usuario/routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from "fastify";
 import { criar } from "./criar.js";
+import { signin } from "./signin.js";
 
 
 export async function usuarioRoutes(app: FastifyInstance){
-    app.post('/usuarios', criar)
+    app.post('/usuarios', criar);
+    app.post("/user/signin", signin);
 }
