b0261ca0e13a3eb360547aacf8a942dcebf083f5
Danilo <daniloluglisantos@gmail.com>
2026-06-30 19:43:24 -0300

    arquivo validador JWT

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { postRoutes } from '@/http/controllers/post/routes.js'
 import { usuarioRoutes } from './http/controllers/usuario/routes.js'
 import fastifyJwt from '@fastify/jwt';
 import { env } from './env/index.js';
+import { validateJwt } from './http/middlewares/jwt.validate.js';
 
 export const app = fastify();
 
@@ -10,6 +11,8 @@ app.register(fastifyJwt, {
     secret: env.JWT_SECRET,
     sign: { expiresIn: '10m' }
 });
+
+app.addHook('onRequest', validateJwt);
 
 app.register(postRoutes);
 app.register(usuarioRoutes);
